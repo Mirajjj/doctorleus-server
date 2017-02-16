a2ed4e9d6df9d89ca8b2cd7f474ab61361c6d030
German Leus <German.Leus@dataart.com>
2017-02-16 18:46:30 +0200

    fixed port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', [
       '127.0.0.1',
-      '207.154.214.56',
+      '207.154.214.56:8000',
       '192.168.169.101'
     ]);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
